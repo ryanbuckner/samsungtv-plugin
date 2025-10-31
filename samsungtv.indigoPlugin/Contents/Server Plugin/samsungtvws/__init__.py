fa0d3ff8c6b84e18b3ceb55cecc174751584197a
ryanbuckner <ryanbuckner@gmail.com>
2025-10-31 14:30:40 -0400

    2025.1.0
    Removed library files and added requirements

--- samsungtv.indigoPlugin/Contents/Server Plugin/samsungtvws/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-SamsungTVWS - Samsung Smart TV WS API wrapper
-
-Copyright (C) 2019 DSR! <xchwarze@gmail.com>
-
-SPDX-License-Identifier: GPL-2.0
-"""
-from .remote import SamsungTVWS
-from .shortcuts import SamsungTVShortcuts
